6c9b9dc13ed1cf85ab12dd33eb91097dfb17f8bb
apazayeferson422-wq <apazayeferson422@gmail.com>
2026-09-16 07:39:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const CALIP_CONFIG={SUPABASE_URL:"https://waojqithcqqlptgpwewa.supabase.co",SUPABASE_PUBLISHABLE_KEY:"sb_publishable_0rh00NFY3ikJrxXg9RYXRg_rB-pMYzI"};
+window.CALIP_CONFIG = {SUPABASE_URL:"https://waojqithcqqlptgpwewa.supabase.co",SUPABASE_PUBLISHABLE_KEY:"sb_publishable_0rh00NFY3ikJrxgX9RYXRg_rB-pMYzI"};
