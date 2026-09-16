9b82abf27a1079b0556be1d5eba1516539bb9840
apazayeferson422-wq <apazayeferson422@gmail.com>
2026-09-16 07:42:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const cfg = window.CALIP_CONFIG || {};
+const cfg = window.CALIP_CONFIG || { SUPABASE_URL: "https://waojqithcqqlptgpwewa.supabase.co", SUPABASE_PUBLISHABLE_KEY: "sb_publishable_0rh00NFY3ikJrxgX9RYXRg_rB-pMYzI" };
 
 let sb = null;
 let products = [];
